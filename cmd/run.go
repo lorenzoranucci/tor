fedd6b285638774ba823f7cac8c40e09ed956c2d
lorenzoranucci <lorenzofranco.ranucci@gmail.com>
2022-11-11 14:42:32 +0100

    setup cobra+viper

---
+++ cmd/run.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run the application",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("run called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+}
